107305a2dbde9efbb225a69f01cc9cf80e88ba6b
Raj <sukhraj5@mail.fresnostate.edu>
2018-11-03 15:22:05 -0700

    Fixed bug with tabs, added forms

--- mainApp/views.py
+++ mainApp/views.py
@@ -8,8 +8,19 @@ from .forms import RegisterForm
 
 # Create your views here.
 def index(request):
-	
-	login_form = LoginForm()
-	register_form = RegisterForm()
 
-	return render(request, 'mainApp/index.html')
+	username = "not logged in"
+
+
+	if request.method == 'POST':
+	        login_form = LoginForm(request.POST)
+	        register_form = RegisterForm(request.POST)
+	        if form.is_valid():
+	            pass  # does nothing, just trigger the validation
+	else:
+		login_form = LoginForm()
+		register_form = RegisterForm()
+	return render(request, 'mainApp/index.html', {'login_form' : login_form, 'register_form':register_form, 'username':username})
+
+
+	   
