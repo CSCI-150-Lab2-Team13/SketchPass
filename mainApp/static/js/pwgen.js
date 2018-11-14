5c828a9f40ba86001973904dcd64b6b5b3a145b4
Mvalov <43051717+Mvalov@users.noreply.github.com>
2018-11-13 19:43:58 -0800

    small bug fixes, UI change, code readability

--- mainApp/static/js/pwgen.js
+++ mainApp/static/js/pwgen.js
@@ -10,13 +10,17 @@ function pwgeneration() {
     var sc = document.getElementById('symcase').checked;
     var items = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]; //no " due to string syntax issue in js
     var Choice = [uc, lc, nc, sc];
-    if (size > 100 || size < 1) alert("Please enter a password length between 1 and 100")
+    if (size > 100 || size < 1) { alert("Please enter a password length between 1 and 100");
+    } 
     else {
-        if (uc + lc + nc + sc == 0) alert("Please choose at least a single checkbox to assist in password generation");
+        if (uc + lc + nc + sc == 0) { alert("Please choose at least a single checkbox to assist in password generation");
+        }
         else { 
-        for (var i = 0; i <= 3; i++) if (Choice[i] == 1) use += items[i];
-        for (var i = 0; i < size; i++) final += use.charAt(Math.floor(Math.random() * use.length));
-        return final;
+            for (var i = 0; i <= 3; i++) { if (Choice[i] == 1) use += items[i];
+            }
+            for (var i = 0; i < size; i++) { final += use.charAt(Math.floor(Math.random() * use.length));
+            }
+         return alert(final);    //final is the end string after pwgeneration has been executed
         }
     }
 };
